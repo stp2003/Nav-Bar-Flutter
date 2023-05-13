7d9a846d4a7e23fa8ab957ce942c0e5857fb4e45
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-05-13 11:43:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:side_nav_bar/constants/colors.dart';
